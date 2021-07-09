75f8b0a81987e29888dc9c4290c6d6c420d31662
Abraham <aperezpe01@gmail.com>
2021-07-09 14:31:52 -0500

    sort specialEvents by uid to save on user reads + only send if user has FMC token

--- index.js
+++ index.js
@@ -32,18 +32,6 @@ in7Days.setHours(0, 0, 0, 0);
 in14Days.setHours(0, 0, 0, 0);
 in30Days.setHours(0, 0, 0, 0);
 
-function createBody(specialEvent, days) {
-  const messagByGender = {
-    Male: `${specialEvent.friend_name}'s ${specialEvent.event_name} is in ${days} days! Buy him a gift already and have peace of mind (:`,
-    Female: `${specialEvent.friend_name}'s ${specialEvent.event_name} is in ${days} days! Buy her a gift already and have peace of mind (:`,
-    Other: `${specialEvent.friend_name}'s ${specialEvent.event_name} is in ${days} days! Buy a gift already and have peace of mind (:`,
-  };
-
-  return days == 0
-    ? `${specialEvent.friend_name}'s ${specialEvent.event_name} is today! (:`
-    : messagByGender[specialEvent.gender];
-}
-
 // Correct remaining days means if it remains 30,14,7 or 0 days for the event
 // to happen DISRIGARDING the year
 
@@ -76,35 +64,46 @@ function hasCorrectRemainingDays(specialEvent, days) {
     );
   }
 }
+// Object to save users who notifications were sent (for development)
+var uids = [];
 // Global variables of uid, user, and tokens to save Firestore reads
 var uid;
 var user;
 var tokens = [];
 // Sends notification if ramianing days match exactly 30,14,7, or 0
 async function sendNotification(specialEvent, days) {
-  if (hasCorrectRemainingDays(specialEvent, days)) {
-    try {
-      if (!user) {
-        uid = specialEvent.uid;
-        user = await db.collection("users").doc(uid).get();
-        tokens = user.data().tokens;
-      } else if (uid != specialEvent.uid) {
-        user = await db.collection("users").doc(uid).get();
-        tokens = user.data().tokens;
-      }
-
+  var isNewUser = false;
+  try {
+    if (!user) {
+      uid = specialEvent.uid;
+      user = await db.collection("users").doc(uid).get();
+      tokens = user.data().tokens;
+      isNewUser = true;
+    } else if (uid != specialEvent.uid) {
+      uid = specialEvent.uid;
+      user = await db.collection("users").doc(uid).get();
+      tokens = user.data().tokens;
+      isNewUser = true;
+    }
+    if (tokens) {
+      if (isNewUser) uids.push(uid);
       const message = {
         notification: {
-          title: "Reminder!!",
-          body: createBody(specialEvent, days),
+          title:
+            days == 0
+              ? `${specialEvent.friend_name}'s ${specialEvent.event_name} is today!`
+              : `${specialEvent.friend_name}'s ${specialEvent.event_name}`,
+          body:
+            days == 0
+              ? "Check out gift ideas!"
+              : `${days} days remaining! Check out gift ideas for ${specialEvent.friend_name}`,
         },
         tokens: tokens,
       };
-
       admin.messaging().sendMulticast(message);
-    } catch (e) {
-      throw e;
     }
+  } catch (e) {
+    throw e;
   }
 }
 
@@ -124,7 +123,7 @@ app.get("/send_notifications", async (req, res) => {
   }
   try {
     // Builds Object of Root Special Events from Firebase
-    const specialEvents = {};
+    const specialEvents = [];
     const todayMonth = today.getMonth();
     await db
       .collection("root_special_events")
@@ -132,25 +131,38 @@ app.get("/send_notifications", async (req, res) => {
       .where("month", "<=", todayMonth + 2)
       .get()
       .then((snapshot) =>
-        snapshot.forEach((event) => (specialEvents[event.id] = event.data()))
+        snapshot.forEach((event) => {
+          var obj = event.data();
+          obj.id = event.id;
+          specialEvents.push(obj);
+        })
       );
 
+    // Sort by uid to save in user reads to Firestore
+    specialEvents.sort((a, b) => (a.uid > b.uid ? 1 : -1));
+
     let count = 0;
-    for (let event_id in specialEvents) {
+    for (let event of specialEvents) {
       // Sends notification if the event is Active AND has exactly 30,14,7, or 0 days remianing
       if (
-        specialEvents[event_id].one_time_event == false ||
-        specialEvents[event_id].date >= today.getTime() * 1000 // To get milliseconds and match Unix Milliseconds Epoch format in DB
+        event.one_time_event == false ||
+        event.date >= today.getTime() * 1000 // To get milliseconds and match Unix Milliseconds Epoch format in DB
       ) {
         count++;
-        await sendNotification(specialEvents[event_id], 30);
-        await sendNotification(specialEvents[event_id], 14);
-        await sendNotification(specialEvents[event_id], 7);
-        await sendNotification(specialEvents[event_id], 0);
+        if (hasCorrectRemainingDays(event, 30))
+          await sendNotification(event, 30);
+        else if (hasCorrectRemainingDays(event, 14))
+          await sendNotification(event, 14);
+        else if (hasCorrectRemainingDays(event, 7))
+          await sendNotification(event, 7);
+        else if (hasCorrectRemainingDays(event, 0))
+          await sendNotification(event, 0);
       }
     }
 
-    console.log(`${count} Notifications successfully sent!!`);
+    console.log("Notifications successfully sent to the following UIDs:");
+    uids.forEach((uid) => console.log(uid));
+
     res.status(200).send(`Notifications Successfully Sent!!!!`);
   } catch (error) {
     console.log(error);
